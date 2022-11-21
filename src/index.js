6889d11d77ec68ec2d09fb29924e5f3e494db0a8
tbrincatos <tamsinbrincatos@gmail.com>
2022-11-21 22:27:09 +0100

    probb with alert

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 function changeCountry(event) {
-  let franceDate = moment().tz("Europe/Paris").format("");
-  if (event.target.value === "france") {
-    alert;
+  let parisDate = moment()
+    .tz("Europe/Paris")
+    .format("[It is] dddd, MMMM D, YYYY hh:mm A [in Europe/Paris]");
+  let sydneyDate = moment()
+    .tz("Australia/Sydney")
+    .format("[It is] dddd, MMMM D, YYYY hh:mm A [in Australia/Sydney]");
+  let newYorkDate = moment()
+    .tz("America/New_York")
+    .format("[It is] dddd, MMMM D, YYYY hh:mm A [in America/New_York]");
+
+  if (event.target.value === "paris") {
+    alert(parisDate);
+  } else if (event.target.value === "sydney") {
+    alert(sydneyDate);
+  } else if (event.target.value === "new-york") {
+    alert(newYorkDate);
   }
 }
 
 let countryElement = document.querySelector("#countries");
+
 countryElement.addEventListener("change", changeCountry);
